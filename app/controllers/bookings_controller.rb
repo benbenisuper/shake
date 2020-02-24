fc2163074f3caf2818bebaf13b448501d96b0802
morgan hoare <hoaremorgan@gmail.com>
2020-02-24 18:19:02 -0300

    Added booking controller methods and validations

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,52 @@
 class BookingsController < ApplicationController
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @user = current_user
+    @booking.user = @user
+    @booking.venue = @venue
+
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def patch
+    @booking = Booking.find(booking_params)
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render :edit
+    end
+  end
+
+  def delete
+    @booking = Booking.find(booking_params)
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :comment, :status)
+  end
+
+
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,9 @@ class Booking < ApplicationRecord
   belongs_to :venue
   has_one :review
 
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :comment, presence: true
+  validates :status, presence: true
+
 end
