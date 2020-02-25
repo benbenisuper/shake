c4983145a7d9da6b1510f982c9a89662b66ba075
morgan hoare <hoaremorgan@gmail.com>
2020-02-25 11:32:18 -0300

    Changed one photo to Many photos for cloudinary

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,7 +1,7 @@
 class Venue < ApplicationRecord
   belongs_to :user
   has_many :bookings
-  has_one_attached :photo
+  has_many_attached :photos
 
   validates :name, presence: :true
   validates :category, presence: :true
