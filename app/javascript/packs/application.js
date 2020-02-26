8cecb32a5eb1bcbacd19e27a4e532117733ae829
Manuel Scholz <manuscho@live.de>
2020-02-26 12:08:26 -0300

    Big Filter Update

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,12 @@
 import "bootstrap";
 import "../plugins/flatpickr";
+import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+
+import { initMapbox } from '../plugins/init_mapbox';
+
+initMapbox();
+
+import { initAutocompleteLocation } from "../plugins/init_autocomplete";
+import { initAutocompleteCity } from "../plugins/init_autocomplete";
+initAutocompleteLocation();
+initAutocompleteCity();
