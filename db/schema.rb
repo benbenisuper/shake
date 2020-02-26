91a5d630028d6b9e20e8ae2e9087b4cbb8a61790
morgan hoare <hoaremorgan@gmail.com>
2020-02-26 18:40:35 -0300

    Added reviews table, model, controller (still needs working) and modified the confirmation page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_140745) do
+ActiveRecord::Schema.define(version: 2020_02_26_193758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 2020_02_26_140745) do
   create_table "reviews", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "booking_id"
-    t.integer "ranting"
+    t.integer "rating"
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
