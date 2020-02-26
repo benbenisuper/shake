b34905e6424ce88e0d7f4738a61cee6d388c397b
Jose Jerez <joseignaciojerez@gmail.com>
2020-02-26 12:32:44 -0300

    map

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -3,52 +3,52 @@ class VenuesController < ApplicationController
   before_action :authenticate_user!
   skip_before_action :authenticate_user!, only: [:index, :show]
 
-def new
-  @venue = Venue.new
+  def new
+    @venue = Venue.new
 
-  authorize @venue
-end
-
-def create
-  @venue = Venue.new(venue_params)
-
-  authorize @venue
-
-  @user = current_user
-  @venue.user = @user
-
-  if @venue.save
-    redirect_to dashboard_path
-  else
-    render :new
+    authorize @venue
   end
-end
 
-def index
-  @location = params["location"]
-  @location = "City" if @location == "" || @location == nil
-  @category = params["Category"]
-  @activity = params["Type-of-event"]
+  def create
+    @venue = Venue.new(venue_params)
 
-  item = Venue.arel_table
+    authorize @venue
 
-  if @location == "City"
-    @venues = policy_scope(Venue).order(created_at: :desc).where(["category = ? and activity = ?", @category, @activity])
-  else
-    @venues = policy_scope(Venue).order(created_at: :desc).where(["category = ? and activity = ?", @category, @activity])
+    @user = current_user
+    @venue.user = @user
 
-    @venues = @venues.where(["location like ?", "%#{@location}%"])
+    if @venue.save
+      redirect_to dashboard_path
+    else
+      render :new
+    end
   end
+
+  def index
+    @location = params["location"]
+    @location = "City" if @location == "" || @location == nil
+    @category = params["Category"]
+    @activity = params["Type-of-event"]
+
+    item = Venue.arel_table
+
+    if @location == "City"
+      @venues = policy_scope(Venue).order(created_at: :desc).where(["category = ? and activity = ?", @category, @activity])
+    else
+      @venues = policy_scope(Venue).order(created_at: :desc).where(["category = ? and activity = ?", @category, @activity])
+
+      @venues = @venues.where(["location like ?", "%#{@location}%"])
+    end
 
 
   @venues_geocoded= @venues.geocoded #returns flats with coordinates
 
-    @markers = @venues_geocoded.map do |venue|
-      {
-        lat: venue.latitude,
-        lng: venue.longitude
-      }
-    end
+  @markers = @venues_geocoded.map do |venue|
+    {
+      lat: venue.latitude,
+      lng: venue.longitude
+    }
+  end
 end
 
 def show
@@ -56,6 +56,39 @@ def show
   @booking = @venue.bookings.build
 
   authorize @venue
+
+
+    item = Venue.arel_table
+
+    if @location == "City"
+      @venues = policy_scope(Venue).order(created_at: :desc).where(["category = ? and activity = ?", @category, @activity])
+    else
+      @venues = policy_scope(Venue).order(created_at: :desc).where(["category = ? and activity = ?", @category, @activity])
+
+      @venues = @venues.where(["location like ?", "%#{@location}%"])
+    end
+
+
+  @venues_geocoded= @venues.geocoded #returns flats with coordinates
+
+  @markers = @venues_geocoded.map do |venue|
+    {
+      lat: venue.latitude,
+      lng: venue.longitude
+    }
+  end
+
+
+# @venue_geocoded= @venue.geocoded #returns flats with coordinates
+
+# @markers = @venue_geocoded.map do |venue|
+#   {
+#     lat: venue.latitude,
+#     lng: venue.longitude
+#   }
+# end
+
+
 end
 
 def delete
